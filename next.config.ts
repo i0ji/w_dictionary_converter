0b7f4c035416e443bc91c95cb91f9f3c31589470
ioji <ioji@inobx.ru>
2024-11-10 22:36:38 +0300

    fix: alias/configs issue

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  //FIXME
+  turbo: {},
   distDir: 'build',
   reactStrictMode: true,
   trailingSlash: true,
   output: 'export',
-  basePath: '/w_dictionary_converter',
-  assetPrefix: '/w_dictionary_converter/'
+  //FIXME
+  // basePath: '/w_dictionary_converter',
+  assetPrefix: '.',
+  webpack: (config) => {
+    config.resolve.alias['@styles'] = 'src/styles/*';
+    return config;
+  }
 };
 
 export default nextConfig;
