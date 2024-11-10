4ce9d2d241e56356e24d47773ba0e4b00fd20f6c
ioji <ioji@inobx.ru>
2024-11-10 20:20:26 +0300

    fix: add build

---
+++ build/_next/static/chunks/app/layout-d6aa6383d44a8153.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3834:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9415,23))},9415:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[224,952,51,358],()=>s(3834)),_N_E=e.O()}]);
