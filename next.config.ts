1d401bc6bd560899eff7156336ce4caaf15adff5
i0ji <ioji@inbox.ru>
2024-12-05 14:33:57 +0300

    deployment test

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,18 @@
 import type { NextConfig } from 'next';
 
-// const nextConfig: NextConfig = {
-//   //FIXME
-//   turbo: {},
-//   distDir: 'build',
-//   reactStrictMode: true,
-//   trailingSlash: true,
-//   output: 'export',
-//   //FIXME
-//   basePath:
-//     process.env.NODE_ENV === 'production' ? '/w_dictionary_converter' : '',
-//   assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
-//   webpack: (config) => {
-//     config.resolve.alias['@styles'] = 'src/styles/*';
-//     return config;
-//   }
-// };
-
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  turbo: {},
-  distDir: 'build',
   reactStrictMode: true,
-  trailingSlash: true,
   output: 'export',
+  images: {
+    unoptimized: true
+  },
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   basePath: isProd ? '/w_dictionary_converter' : '',
   assetPrefix: isProd ? '/w_dictionary_converter/' : '',
   webpack: (config) => {
-    config.resolve.alias['@styles'] = 'src/styles/*';
+    config.resolve.alias['@styles'] = './src/styles/*';
     return config;
-  },
-  sassOptions: {
-    implementation: 'sass-embedded',
-    additionalData: `$var: red;`,
-  },
+  }
 };
-
-export default nextConfig;
