2dfba6c66da2feaf30d5e7c1bbf8e0f98744ee60
i0ji <ioji@inbox.ru>
2025-03-23 21:35:43 +0300

    chore: tailwind issues

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+    purge: {
+      enabled: process.env.NODE_ENV === 'production',
+      content: [
+        './src/**/*.html',
+        './src/**/*.vue',
+        './src/**/*.jsx',
+      ],
+    },
+  }
